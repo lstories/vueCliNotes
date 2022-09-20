4628aa04dbddd11bf2de79d9a6b4ea496ce08288
lstories <lstories@163.com>
2022-09-20 23:57:52 +0800

    VueLearning

---
+++ 07. src_TodoList案例/App.vue
@@ -0,0 +1,110 @@
+<template>
+  <div id="root">
+    <div class="todo-container">
+      <div class="todo-wrap">
+        <MyHeader :addTodo="addTodo"/>
+        <MyList :todos="todos" :checkTodo="checkTodo" :deleteTodo="deleteTodo"/>
+        <MyFooter :todos="todos" :checkAllTodo="checkAllTodo" :clearAllTodo="clearAllTodo"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+    import MyHeader from './components/MyHeader.vue'
+    import MyList from './components/MyList.vue'
+    import MyFooter from './components/MyFooter.vue'
+
+    export default {
+        name:"App",
+        // 汇总所有的组件
+        components: {
+            MyHeader,
+            MyFooter,
+            MyList
+        },
+        data() {
+            return {
+                todos: [
+                    { id: '001', title: '吃饭', done: false },
+                    { id: '002', title: '游戏', done: true },
+                    { id: '003', title: '喝酒', done: false },
+                ]
+            }
+        },
+        methods: {
+            // 添加的todo
+            addTodo(todo){
+                console.log('我是app组件，我收到了数据');
+                this.todos.unshift(todo);
+            },
+            // 勾选or取消勾选一个todo
+            checkTodo(id){
+                const todo = this.todos.find( todo => todo.id === id);
+                todo.done = !todo.done;
+            },
+            // 删除一个todo
+            deleteTodo(id){
+                this.todos = this.todos.filter((todo)=>{
+                    return todo.id !== id
+                })
+            },
+            // 全选or取消全选
+            checkAllTodo(done){
+                this.todos.forEach((todo) => {
+                  this.todos.forEach(todo => todo.done = done);
+                })
+            },
+            // 删除
+            clearAllTodo(){
+                this.todos = this.todos.filter(todo => !todo.done)
+            }
+        }
+        
+    }
+</script>
+
+<style>
+/*base*/
+body {
+  background: #fff;
+}
+
+.btn {
+  display: inline-block;
+  padding: 4px 12px;
+  margin-bottom: 0;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: center;
+  vertical-align: middle;
+  cursor: pointer;
+  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
+  border-radius: 4px;
+}
+
+.btn-danger {
+  color: #fff;
+  background-color: #da4f49;
+  border: 1px solid #bd362f;
+}
+
+.btn-danger:hover {
+  color: #fff;
+  background-color: #bd362f;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
+</style>
