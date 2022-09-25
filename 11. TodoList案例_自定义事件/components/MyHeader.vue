f646dfc576d62a41a68a371a7b1f4efb8416329e
lstories <lstories@163.com>
2022-09-25 23:30:43 +0800

    TodoList案例_自定义事件

---
+++ 11. TodoList案例_自定义事件/components/MyHeader.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="todo-header">
+        <input type="text" placeholder="请输入你的任务名称，按回车键确认" v-model="title" @keyup.enter="add"/>
+    </div>
+</template>
+
+<script>
+    import {nanoid} from 'nanoid';
+    export default {
+        //注意不管是你写的data也还还是methods也好，甚至是computed计算属性也好都会出现在组件事例对象vc身上
+        //属性值不能重名
+        name: 'MyHeader',
+        data() {
+            return {
+                title: ''
+            }
+        },
+        methods:{
+            add(){
+                console.log(this.title)
+                // 判断, 校验数据, trim()去掉空格
+                if (!this.title.trim()){
+                    alert('待办事项不得为空')
+                    return; // 输入的待办事项为空则不走下面流程
+                }
+                // 将用户的输入包装成一个todo对象
+                const todoObj = {
+                    id: nanoid(),
+                    title: this.title,
+                    done: false
+                }
+                
+                //console.log(todoObj);
+                // 通知App组件去添加一个todo对象
+                this.$emit('addTodo')
+                //this.addTodo(todoObj);
+                // 清空输入
+                this.title = '';
+            }
+        },
+
+
+    }
+</script>
+
+<style scoped>
+/*header*/
+.todo-header input {
+  width: 560px;
+  height: 28px;
+  font-size: 14px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  padding: 4px 7px;
+}
+
+.todo-header input:focus {
+  outline: none;
+  border-color: rgba(82, 168, 236, 0.8);
+  box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075), 0 0 8px rgba(82, 168, 236, 0.6);
+}
+</style>
