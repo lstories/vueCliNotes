dfa40cd911a64bff545ee38748f65d9455d87118
lstories <lstories@163.com>
2022-09-27 19:38:04 +0800

    多个元素过渡效果

---
+++ 18. 多个元素过渡效果/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Test></Test>
+    <Test2/>
+  </div>
+</template>
+
+<script>
+import Test from './components/Test.vue';
+import Test2 from './components/Test2.vue';
+    
+    export default {
+        name:"App",
+        // 汇总所有的组件
+        components: {
+          Test,
+          Test2
+        },
+        
+    }
+</script>
+
+<style>
+
+</style>
